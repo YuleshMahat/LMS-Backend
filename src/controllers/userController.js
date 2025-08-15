2f64116288e38ded14ae6e6261318d9bb971812a
Yulesh Mahat <yuleshkazuto@gmail.com>
2025-08-15 21:26:01 +1000

    added refresh token middleware and sent the appropriate error messages

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,6 +1,6 @@
 export const getUserDetails = async (req, res) => {
   console.log("Call back funcion triggered after middleware", req.user);
-  res.send({
+  res.status(200).json({
     status: true,
     user: req.user,
     message: "User detail found",
--- src/middleware/authmiddleware.js
+++ src/middleware/authmiddleware.js
@@ -1,5 +1,6 @@
 import jwt from "jsonwebtoken";
 import { getUserByEmail } from "../models/users/userModel.js";
+import { createAccessToken } from "../utils/jwt.js";
 
 export const authmiddleware = async (req, res, next) => {
   console.log("Authentication middleware triggered.");
@@ -19,9 +20,42 @@ export const authmiddleware = async (req, res, next) => {
       });
     }
   } catch (err) {
-    return res.status(401).json({
+    let errorMessage = err.message.includes("jwt expire")
+      ? "Expired access token"
+      : "Server Error";
+
+    let status = err.message.includes("jwt expire") ? 401 : 500;
+    return res.status(status).json({
       status: false,
-      message: "Unauthorized!!",
+      message: errorMessage,
+    });
+  }
+};
+
+export const refreshmiddleware = async (req, res, next) => {
+  console.log("Resresh token middleware trigerred");
+  try {
+    let refreshToken = req.headers.authorization;
+    let decoded = jwt.verify(refreshToken, process.env.JWT_REFRESH_SECRET);
+    let user = await getUserByEmail(decoded.email);
+    if (user) {
+      user.password = "";
+      req.user = user;
+      next();
+    } else {
+      return res.status(401).json({
+        status: false,
+        message: "Unauthorized!!",
+      });
+    }
+  } catch (err) {
+    let errorMessage = err.message.includes("jwt expire")
+      ? "Expired refresh token"
+      : "Server Error";
+    let status = err.message.includes("jwt expire") ? 401 : 500;
+    return res.status(status).json({
+      status: false,
+      message: errorMessage,
     });
   }
 };
