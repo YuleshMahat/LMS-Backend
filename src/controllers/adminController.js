1c95f55911ea6e1630aa2f19443ca0745c9ef9bc
Yulesh Mahat <yuleshkazuto@gmail.com>
2025-09-19 21:27:16 +1000

    Added delete user functionality

--- src/controllers/adminController.js
+++ src/controllers/adminController.js
@@ -1,4 +1,5 @@
 import {
+  deleteUser,
   getUserById,
   getUsers,
   updateUser,
@@ -11,6 +12,19 @@ export const getAdmins = async (req, res) => {
       res
         .status(200)
         .json({ status: true, message: "Fetched admin users", admins });
+    }
+  } catch (error) {
+    res.status(500).json({ status: false, message: "Internal server error" });
+  }
+};
+
+export const fetchUsers = async (req, res) => {
+  try {
+    const users = await getUsers();
+    if (users) {
+      res
+        .status(200)
+        .json({ status: true, message: "Fetched all users", users });
     }
   } catch (error) {
     res.status(500).json({ status: false, message: "Internal server error" });
@@ -36,6 +50,23 @@ export const changeAdminStaus = async (req, res) => {
     }
 
     res.status(200).json({ status: true, message: "Updated admin status" });
+  } catch (error) {
+    res.status(500).json({ status: false, message: "Internal server error" });
+  }
+};
+
+export const deleteAccount = async (req, res) => {
+  try {
+    const id = req.body._id;
+    const admin = await deleteUser(id);
+
+    if (!admin) {
+      return res.status(404).json({ status: false, message: "User not found" });
+    }
+
+    res
+      .status(200)
+      .json({ status: true, message: "Successfully deleted user" });
   } catch (error) {
     res.status(500).json({ status: false, message: "Internal server error" });
   }
--- src/routes/adminRouter.js
+++ src/routes/adminRouter.js
@@ -1,11 +1,20 @@
 import express from "express";
-import { changeAdminStaus, getAdmins } from "../controllers/adminController.js";
+import {
+  changeAdminStaus,
+  getAdmins,
+  fetchUsers,
+  deleteAccount,
+} from "../controllers/adminController.js";
 import { authmiddleware, isAdmin } from "../middleware/authmiddleware.js";
 
 const router = express.Router();
 
 router.get("/", authmiddleware, isAdmin, getAdmins);
 
+router.get("/users", authmiddleware, isAdmin, fetchUsers);
+
 router.post("/", authmiddleware, isAdmin, changeAdminStaus);
+
+router.post("/users", authmiddleware, isAdmin, deleteAccount);
 
 export default router;
